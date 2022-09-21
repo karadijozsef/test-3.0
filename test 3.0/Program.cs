4762291ca8e69b8409d37e7aac77e5a9a253e7c2
cisco <cisco@DESKTOP-D4D1G54>
2022-09-21 13:06:56 +0200

    Add project files.

---
+++ test 3.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_3._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adjon meg 2 darab számot!");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+            if (szam1 > szam2)
+            {
+                Console.WriteLine("Az első a nagyobb szám!");
+            }
+            else
+            {
+                Console.WriteLine("A második a nagyobb szám!");
+            }
+            Console.WriteLine("\nProgram vége!");
+            Console.ReadKey();
+        }
+    }
+}
